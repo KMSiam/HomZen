b5cad928fc83a24a12049a5f21e886fbf83c1bf8
KMSiam <k.m.siam2019@gmail.com>
2025-08-22 17:18:56 +0600

    Add Alart Dialog Box to got out from app and update navigation

--- app/src/main/java/com/kmsiam/seu/isd/lab/project/homzen/Fragment/HomeFragment.java
+++ app/src/main/java/com/kmsiam/seu/isd/lab/project/homzen/Fragment/HomeFragment.java
@@ -2,6 +2,10 @@ package com.kmsiam.seu.isd.lab.project.homzen.Fragment;
 
 import android.os.Bundle;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,57 +14,36 @@ import android.view.ViewGroup;
 
 import com.kmsiam.seu.isd.lab.project.homzen.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public HomeFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Handle back button press
+        requireActivity().getOnBackPressedDispatcher().addCallback(
+                getViewLifecycleOwner(),
+                new OnBackPressedCallback(true) {
+                    @Override
+                    public void handleOnBackPressed() {
+                        new AlertDialog.Builder(requireContext())
+                                .setTitle("Exit App")
+                                .setIcon(R.drawable.ic_logout)
+                                .setMessage("Are you sure you want to exit?")
+                                .setPositiveButton("Yes", (dialog, which) -> requireActivity().finishAffinity())
+                                .setNegativeButton("No", (dialog, which) -> dialog.dismiss())
+                                .show();
+                    }
+                }
+        );
     }
 }
--- app/src/main/java/com/kmsiam/seu/isd/lab/project/homzen/MainActivity.java
+++ app/src/main/java/com/kmsiam/seu/isd/lab/project/homzen/MainActivity.java
@@ -1,11 +1,7 @@
 package com.kmsiam.seu.isd.lab.project.homzen;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.MenuItem;
-
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,7 +11,6 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 import com.kmsiam.seu.isd.lab.project.homzen.Fragment.CartFragment;
 import com.kmsiam.seu.isd.lab.project.homzen.Fragment.GroceryFragment;
 import com.kmsiam.seu.isd.lab.project.homzen.Fragment.HomeFragment;
@@ -24,13 +19,12 @@ import com.kmsiam.seu.isd.lab.project.homzen.Fragment.ServiceFragment;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView btnNavigationView;
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -42,22 +36,47 @@ public class MainActivity extends AppCompatActivity {
         btnNavigationView.setOnItemSelectedListener(menuItem -> {
             int id = menuItem.getItemId();
 
+            Fragment current = getSupportFragmentManager().findFragmentById(R.id.FragmentContainer);
+
             if (id == R.id.nav_Home) {
+                if (current instanceof HomeFragment) return true;// avoid duplicate replace
                 loadFrag(new HomeFragment(), true);
             } else if (id == R.id.nav_Grocery) {
+                if (current instanceof GroceryFragment) return true;
                 loadFrag(new GroceryFragment(), false);
             } else if (id == R.id.nav_Cart) {
+                if (current instanceof CartFragment) return true;
                 loadFrag(new CartFragment(), false);
             } else if (id == R.id.nav_Service) {
+                if (current instanceof ServiceFragment) return true;
                 loadFrag(new ServiceFragment(), false);
-            } else { //profile
+            } else { // profile
+                if (current instanceof ProfileFragment) return true;
                 loadFrag(new ProfileFragment(), false);
             }
-            return true; // Return true to display the item as the selected item
+            return true;
         });
 
+        // Default selected item
         btnNavigationView.setSelectedItemId(R.id.nav_Home);
+
+        //When back stack changes, sync the checked item to the current fragment
+        getSupportFragmentManager().addOnBackStackChangedListener(() -> {
+            Fragment f = getSupportFragmentManager().findFragmentById(R.id.FragmentContainer);
+            if (f instanceof HomeFragment) {
+                btnNavigationView.getMenu().findItem(R.id.nav_Home).setChecked(true);
+            } else if (f instanceof GroceryFragment) {
+                btnNavigationView.getMenu().findItem(R.id.nav_Grocery).setChecked(true);
+            } else if (f instanceof CartFragment) {
+                btnNavigationView.getMenu().findItem(R.id.nav_Cart).setChecked(true);
+            } else if (f instanceof ServiceFragment) {
+                btnNavigationView.getMenu().findItem(R.id.nav_Service).setChecked(true);
+            } else if (f instanceof ProfileFragment) {
+                btnNavigationView.getMenu().findItem(R.id.nav_Profile).setChecked(true);
+            }
+        });
     }
+
     public void loadFrag(Fragment fragment, boolean flag) {
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
