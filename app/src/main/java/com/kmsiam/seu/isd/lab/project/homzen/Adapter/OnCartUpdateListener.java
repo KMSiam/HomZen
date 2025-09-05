87d389a52f246bcd2c55a48571cc6416b44ce749
KMSiam <k.m.siam2019@gmail.com>
2025-09-05 21:35:42 +0600

    Update login siagnUp Cradintial

--- app/src/main/java/com/kmsiam/seu/isd/lab/project/homzen/Adapter/OnCartUpdateListener.java
+++
@@ -1,8 +0,0 @@
-package com.kmsiam.seu.isd.lab.project.homzen.Adapter;
-
-/**
- * Interface to notify when the cart is updated
- */
-public interface OnCartUpdateListener {
-    void onCartUpdated();
-}
